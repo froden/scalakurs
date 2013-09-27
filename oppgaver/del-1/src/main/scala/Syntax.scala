edd35b95358db4793e974870d179c95fcbbd1af8
Torbjørn Vatn <torbjorn@vatn.org>
2013-09-27 22:15:36 +0200

    Første forsøk på en SyntaxSpec

---
+++ oppgaver/del-1/src/main/scala/Syntax.scala
@@ -0,0 +1,5 @@
+object Syntax {
+  val fixed = 42
+  var reAssignable = "Change me!"
+  def callMe = "Hello!"
+}
---
+++ oppgaver/del-1/src/test/scala/SyntaxSpec.scala
@@ -0,0 +1,24 @@
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+
+class SyntaxSpec extends FlatSpec with ShouldMatchers {
+
+  "Syntaks" should "have a value called fixed" in {
+    assert(Syntax.fixed === 42)
+  }
+
+  it should "have a variable called reAssignable" in {
+    assert(Syntax.reAssignable === "Change me!")
+  }
+
+  it should "have a new value assigned to reAssignable" in {
+    Syntax.reAssignable = "I feel changed!"
+    assert(Syntax.reAssignable === "I feel changed!")
+  }
+
+  it should "have a method we could call" in {
+    assert(Syntax.callMe === "Hello!")
+  }
+
+
+}
