45c142981f0d3b127f583adea088a7f590d02c7c
Torbjørn Vatn <torbjorn@vatn.org>
2013-10-07 22:42:43 +0200

    Function oppgave

--- oppgaver/del-1/src/main/scala/oppg1_intro/SyntaxIntro.scala
+++ oppgaver/del-1/src/main/scala/oppg1_intro/SyntaxIntro.scala
@@ -4,42 +4,48 @@ import scala.io.Source
 
 object SyntaxIntro {
 
-//  // Values and variables are two differnt consepts and
-//  // they (mostly) don't specify a return type…
-//  val fixed = ???
-//
-//  // …but they could
-//  var reAssignable: String = ???
-//
-//  // Methods don't need a parameterlist
-//  // or a return type either.
-//  // Curly braces are optional too
-//  def callMe = ???
-//
-//  def theMeaningOfLife: String = {
-//    "Don't know"
-//  }
-//
-//  //HINT: Add some parameters and a body using them
-//  def add() = 9
-//
-//  def rightLanguage(lang: String) = {
-//    // Everything in Scala is an expression, thus it can
-//    // be assigned to a value.
-//    // E.g. the result of an if/else statement
-//    val theAnswer: String = if(true) ??? else ???
-//    theAnswer
-//  }
-//
-//  var nr = 0
-//  def countToTen() {
-//    // It's not often one would use a for-loop for side
-//    // effects like this, but you could. The range syntax
-//    // is quite nice
-//    for (i <- 1 to ???) {
-//      ???
-//    }
-//  }
+  // Values and variables are two differnt consepts and
+  // they (mostly) don't specify a return type…
+  val fixed = ???
+
+  // …but they could
+  var reAssignable: String = ???
+
+  // Methods don't need a parameterlist
+  // or a return type either.
+  // Curly braces are optional too
+  def callMe = ???
+
+  def theMeaningOfLife: String = {
+    "Don't know"
+  }
+
+  //HINT: Add some parameters and a body using them
+  def add() = 9
+
+  def rightLanguage(lang: String) = {
+    // Everything in Scala is an expression, thus it can
+    // be assigned to a value.
+    // E.g. the result of an if/else statement
+    val theAnswer: String = if(true) ??? else ???
+    theAnswer
+  }
+
+  var nr = 0
+  def countToTen() {
+    // It's not often one would use a for-loop for side
+    // effects like this, but you could. The range syntax
+    // is quite nice
+    for (i <- 1 to ???) {
+      ???
+    }
+  }
+
+  val isXLargerThanY = ???
+
+  def checkResultOfFunction(func: (Int, Int) => Boolean) = {
+    func.apply(2, 4)
+  }
 
   def printSomething() {
     for(line <- Source.fromURL(getClass.getResource("/flower.txt")).getLines())
--- oppgaver/del-1/src/test/scala/oppg1_intro/SyntaxTest.scala
+++ oppgaver/del-1/src/test/scala/oppg1_intro/SyntaxTest.scala
@@ -5,49 +5,54 @@ import org.scalatest._
 class SyntaxTest extends FunSuite {
 
   test ("SyntaxIntro should have a value called fixed") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.fixed === 100)
   }
 
   test ("SyntaxIntro should have a variable called reAssignable") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.reAssignable === "Change me!")
   }
 
   test ("SyntaxIntro should have a new value assigned to reAssignable") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.reAssignable === "I feel changed!")
   }
 
   test ("SyntaxIntro should have a method we could call") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.callMe === "Hello!")
   }
 
   test ("SyntaxIntro should have a method with a wrong return type that we should fix") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.theMeaningOfLife === 42)
   }
 
   test ("SyntaxIntro should have a method for adding two numbers together") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.add() === 7)
   }
 
   test ("SyntaxIntro should have a method for choosing the right language") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    assert(SyntaxIntro.rightLanguage("Scala") === "YES")
 //    assert(SyntaxIntro.rightLanguage("Java") === "NO")
   }
 
   test ("SyntaxIntro should count to ten") {
-    pending //TODO: remove
+    pending //TODO: remove this line and uncomment the line below
 //    SyntaxIntro.countToTen()
 //    assert(SyntaxIntro.nr === 10)
   }
 
-  test ("SyntaxIntro should print something pretty ") {
+  test ("SyntaxIntro should have a method that takes a functions as parameter") {
     pending //TODO: remove
+    SyntaxIntro.checkResultOfFunction(SyntaxIntro.isXLargerThanY)
+  }
+
+  test ("SyntaxIntro should print something pretty ") {
+    pending //TODO: remove this line and uncomment the line below
 //    SyntaxIntro.printSomething()
   }
 
