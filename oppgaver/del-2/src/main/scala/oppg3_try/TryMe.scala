f0568a07f5375781f8d35c85ee97b910245c991d
Torbjørn Vatn <torbjorn@vatn.org>
2013-10-26 20:16:10 +0200

    Filer til Try-oppgavene

---
+++ oppgaver/del-2/src/main/scala/oppg3_try/TryMe.scala
@@ -0,0 +1,7 @@
+package oppg3_try
+
+class TryMe {
+
+
+
+}
---
+++ oppgaver/del-2/src/test/scala/oppg3_try/TryTest.scala
@@ -0,0 +1,10 @@
+package oppg3_try
+
+import org.scalatest.FunSuite
+
+class TryTest extends FunSuite {
+
+  test("increment numbers in a list") {
+    assert(true === true)
+  }
+}
