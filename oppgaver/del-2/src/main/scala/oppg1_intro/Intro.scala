355c02cab94116bd688834d73582c9b1aaf8b19a
Frode Nerbråten <frode@nerbraten.no>
2013-10-28 20:58:57 +0100

    Fjernet surs impl av introtester

--- oppgaver/del-2/src/main/scala/oppg1_intro/Intro.scala
+++ oppgaver/del-2/src/main/scala/oppg1_intro/Intro.scala
@@ -16,26 +16,7 @@ object Intro {
    *        Sjur Frode Arild Torbjørn
    */
   def findPath(name: String, tree: Tree): List[String] = tree match {
-    case n: Node=> {
-      val leftPath: List[String] = findPath(name, n.left)
-      val rightPath: List[String] = findPath(name, n.right)
-      if (leftPath != Nil) {
-        "left" :: leftPath
-      }
-      else if (rightPath != Nil) {
-        "right" :: rightPath
-      }
-      else {
-        Nil
-      }
-    }
-    case l: Leaf => {
-      if(name == l.value) {
-        List(l.value)
-      } else {
-        Nil
-      }
-    }
+    case _ => ???
   }
 
 }
--- oppgaver/del-2/src/test/scala/oppg1_intro/IntroTest.scala
+++ oppgaver/del-2/src/test/scala/oppg1_intro/IntroTest.scala
@@ -7,7 +7,7 @@ import oppg1_intro.Intro.{Node, Leaf}
 class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
 
   /*
-   * Complete this test by replacing all ____ with actual implementation
+   * Complete this test by replacing all __ with actual implementation
    */
   test("Everything is expressions") {
     val parsed = try {
@@ -15,14 +15,14 @@ class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
     } catch {
       case _: NumberFormatException => 0
     }
-    parsed should be(23)
+    parsed should be(____)
 
     val parsed2 = try {
       "hello".toInt
     } catch {
       case _: NumberFormatException => 0
     }
-    parsed2 should be(0)
+    parsed2 should be(____)
   }
 
   /*
@@ -33,13 +33,13 @@ class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
       val greeting = "Hi there " + name
       greeting
     }
-    sayHi("Sjur") should be("Hi there Sjur")
+    sayHi("Sjur") should be(____)
 
     def sayHi2(name: String) = {
       val greeting = "Hi there " + name
       println(greeting)
     }
-    sayHi2("Frode") should be(())
+    sayHi2("Frode") should be(____)
   }
 
   /*
@@ -61,11 +61,11 @@ class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
     //closure
     val add4 = (y: Int) => x + y
 
-    add1(1, 2) should be(3)
-    add2(1, 2) should be(3)
-    add3(1, 2) should be(3)
-    add3.apply(1, 2) should be (3)
-    add4(2) should be(4)
+    add1(1, 2) should be(____)
+    add2(1, 2) should be(____)
+    add3(1, 2) should be(____)
+    add3.apply(1, 2) should be (____)
+    add4(2) should be(____)
   }
 
   /*
@@ -77,10 +77,10 @@ class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
 
     val addTwo = addOneMore(addOne)
 
-    addTwo(2) should be(4)
+    addTwo(2) should be(____)
 
     val addTwoMore = addOneMore andThen addOneMore
-    addTwoMore(addOne)(1) should be(4)
+    addTwoMore(addOne)(2) should be(____)
   }
 
 
@@ -111,10 +111,11 @@ class IntroTest extends FunSuite with ShouldMatchers with ScalakursSupport {
     def applyFunction(value: String, f: (String => Int)): Int = f(value)
 
     val lengthOfString: String => Int = _.length
-    val equalTo5: String => Boolean = _.toInt == 5
+    val equalTo5: Int => Boolean = _ == 5
 
     applyFunction("Hi", lengthOfString) should be(2)
 
-    applyFunction("5", equalTo5) should be(true)
+    pending //remove this line and uncomment the next
+//    applyFunction(5, equalTo5) should be(true)
   }
 }
