d564d8bd54455122ee57ecb6cebbc56013a6941b
Frode Nerbråten <frode@nerbraten.no>
2013-10-29 09:11:16 +0100

    La tilbake LF for Try

--- oppgaver/del-2/src/main/scala/oppg4_try/UrlParser.scala
+++ oppgaver/del-2/src/main/scala/oppg4_try/UrlParser.scala
@@ -10,15 +10,12 @@ object UrlParser {
     Try(new URL(url))
   }
 
-  /** this method needs to have Try[InputStream] as return type. Hint: flatMap can help us with that */
-  def inputStreamForURL(url: String): Try[Try[Try[InputStream]]] = parseURL(url).map { u =>
-    Try(u.openConnection()).map(conn => Try(conn.getInputStream))
+  def inputStreamForURL(url: String): Try[InputStream] = parseURL(url).flatMap { u =>
+    Try(u.openConnection()).flatMap(conn => Try(conn.getInputStream))
   }
 
-  /** this method should be returing an error if the protocol of the URL is something other than http
-    * HINT: read the name of the test testing this method */
   def parseHttpUrl(url: String): Try[URL] = {
-    parseURL(url)
+    parseURL(url).filter(_.getProtocol == "http")
   }
 
   import scala.io.Source
@@ -26,7 +23,7 @@ object UrlParser {
     for {
       url <- parseURL(url)
       connection <- Try(url.openConnection())
-      is <- new Failure(new Throwable) /** this Failure needs to be replaced by a Try(_something_) */
+      is <- Try(connection.getInputStream)
       source = Source.fromInputStream(is)
     } yield source.getLines()
 
--- oppgaver/del-2/src/test/scala/oppg4_try/TryTest.scala
+++ oppgaver/del-2/src/test/scala/oppg4_try/TryTest.scala
@@ -4,66 +4,53 @@ import org.scalatest._
 import oppg4_try.UrlParser._
 import support.ScalakursSupport
 import scala.util.{Try, Failure, Success}
-import java.net.URL
+import java.net.{MalformedURLException, URL}
 import java.io.InputStream
 
 class TryTest extends FunSuite with ShouldMatchers with ScalakursSupport {
 
-  /* Complete these test by replacing ____ with Try(_something_) and ??? with a MalformURLException */
   test("parse an URL") {
-    parseURL("http://www.ntnu.no") should be(____)
-    parseURL("xxx://oooops.com") should beFailureWithException(???)
+    parseURL("http://www.ntnu.no") should be(Try(new URL("http://www.ntnu.no")))
+    parseURL("xxx://oooops.com") should beFailureWithException(new MalformedURLException("unknown protocol: xxx"))
   }
 
-  /** replace the ____ in this test with the correct alternative URL */
   test("visit an alternative URL if the first one fails") {
-    parseURL("oooops").getOrElse(____) should be(new URL("http://www.vg.no"))
+    parseURL("oooops").getOrElse(new URL("http://www.vg.no")) should be(new URL("http://www.vg.no"))
   }
 
-  /** replace ____ inside map() with the correct function */
   test("get the URL´s protocol with map") {
-    parseURL("http://www.ntnu.no").map(____) should be(Try("http"))
+    parseURL("http://www.ntnu.no").map(_.getProtocol) should be(Try("http"))
   }
 
-  /** You have to rewrite inputStreamForURL() using flatMap to avoid the stacking up of Trys */
   test("avoid making a Try train wreck using flatMap") {
 
     val inputStream = inputStreamForURL("http://www.ntnu.no")
-                      //    ⬆︎
-                      // Change this method
 
     inputStream match {
       case Success(stream: InputStream) => stream.read() should be > 0
       case Failure(_) => fail("This test should be successful")
-      case _ => ____
+      case _ => __
     }
   }
 
-  /** parseHttpUrl needs to be smarter so it failes if the protocol is something other than http */
   test("filter out the correct protocol") {
-    // Change this method
-    //   ⬇︎
     parseHttpUrl("http://www.ntnu.no") match {
       case Success(url) => url.getProtocol shouldEqual "http"
-      case Failure(_) => ____
+      case Failure(_) => __
     }
 
     parseHttpUrl("ftp://www.ntnu.no") match {
-      case Success(_) => ____
+      case Success(_) => __
       case Failure(e) => e.getMessage shouldEqual "Predicate does not hold for ftp://www.ntnu.no"
     }
   }
 
-  /** getURLContent needs some rewriting to successfully return a Try[Iterator[String]] */
   test("chaining Trys in a for comprehension") {
-    // Change this method
-    //   ⬇︎
     getURLContent("http://www.ntnu.no") match {
       case Success(iterator) => iterator.size should be > 0
-      case Failure(_) => ____
+      case Failure(_) => __
     }
   }
-
 
 
   def beFailureWithException(t: Throwable) = {
