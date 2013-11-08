de347e2df20d6c7906f70c09ef97f8f3abb64751
Frode Nerbråten <frode@nerbraten.no>
2013-11-08 07:45:13 +0100

    Fungerende innsetting og uthenting av artikler

--- oppgaver/del-3/scalakurs-blogg/src/main/scala/ScalatraBootstrap.scala
+++ oppgaver/del-3/scalakurs-blogg/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,15 @@
+import com.mongodb.casbah.Imports._
 import scalakurs._
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new ArticlesController, "/articles/*")
+
+    val mongoClient = MongoClient()
+    val db = mongoClient("blog")
+    val articles = db("articles")
+
+    context.mount(new ArticlesController(articles), "/*")
   }
 }
--- oppgaver/del-3/scalakurs-blogg/src/main/scala/scalakurs/ArticlesController.scala
+++ oppgaver/del-3/scalakurs-blogg/src/main/scala/scalakurs/ArticlesController.scala
@@ -1,16 +1,31 @@
 package scalakurs
 
 import org.slf4j.LoggerFactory
+import com.mongodb.casbah.Imports._
+import org.json4s.mongo.JObjectParser
+import org.json4s._
+import org.scalatra.{Created, BadRequest}
 
 
-class ArticlesController extends ScalakursbloggStack {
+class ArticlesController(articles: MongoCollection) extends ScalakursbloggStack {
 
   val log = LoggerFactory.getLogger(getClass)
 
   /**
    * ArticlesController is mounted under /articles so the root path here will be /articles/
    */
-  get("/") {
+  get("/articles") {
+    articles.find().flatMap(m => mongoToJs(m).extractOpt[Article]).toList
+  }
+
+  post("/articles") {
+    parsedBody.extractOpt[Article].map { article =>
+      articles.insert(jsToMongo(Extraction.decompose(article)))
+      article
+    } match {
+      case None => BadRequest
+      case Some(article) => Created(article)
+    }
 
   }
 
@@ -19,4 +34,8 @@ class ArticlesController extends ScalakursbloggStack {
     log.info("Got message: " + message)
     message
   }
+
+  def toArticle(obj: DBObject) = mongoToJs(obj).extractOpt[Article]
+  def jsToMongo(value: JValue): DBObject = JObjectParser.parse(value)
+  def mongoToJs(obj: Any): JValue = JObjectParser.serialize(obj)
 }
